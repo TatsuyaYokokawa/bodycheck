ddcbb6c040c6d44b1bcf7d9f037ea46a5a78aeff
TatsuyaYokokawa <beattherich@iCloud.com>
2020-03-20 17:11:30 +0900

    db:create
    user
    food
    bodyinfo

---
+++ db/migrate/20200320080541_bodyinfo.rb
@@ -0,0 +1,16 @@
+class Bodyinfo < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bodyinfo do |t|
+
+      t.date       :date
+      t.integer    :sex
+      t.integer    :age
+      t.integer    :height
+      t.integer    :weight
+      t.references :user, foreign_key: true
+
+      t.timestamps
+
+    end
+  end
+end
